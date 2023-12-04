1cac164847a880b7cead9965acb4a1ca320f7d93
Oscar Andersson <oandersson50@gmail.com>
2023-12-04 19:45:35 +0100

    Upload of advent of code

---
+++ Day1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+namespace day1
+{
+    class day1
+    {
+        static void Main()
+        {
+            string path = @".\textfile.txt";
+            string lines = File.ReadAllText (path);
+          
+            
+    
+           string line = Regex.Replace(lines, "[a-z]", "");
+            
+        
+           
+            Console.WriteLine(line);
+
+
+
+
+
+
+        }
+    }
+}
