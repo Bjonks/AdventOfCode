701495c43a4f4c5f040d1591595651d86334e5af
Bjonks <oandersson50@gmail.com>
2023-12-05 00:27:13 +0100

    Added and tested som code to get the first and last digit

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -7,19 +7,27 @@ namespace day1
     {
         static void Main()
         {
+            
+            
             string path = @".\textfile.txt";
             string lines = File.ReadAllText (path);
           
             
     
            string line = Regex.Replace(lines, "[a-z]", "");
-            
-        
+           List<string> numbers = line.Split('\n').ToList();
            
-            Console.WriteLine(line);
+           foreach (var i in numbers)
+           {
+            int first = Convert.ToInt32(i.Substring(0,1));
+            int second = Convert.ToInt32(i.Substring(last,1));
+           // int second = i.LastIndexOf('.');
+           // int sum = first * 10 + second;
+        
+              Console.WriteLine(second);
+           }
 
-
-
+          
 
 
 
